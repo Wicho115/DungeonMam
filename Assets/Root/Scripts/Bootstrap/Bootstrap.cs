a5105b4088345295bc3a3d3a0cb761635bfd77b2
Dante <61216048+Wicho115@users.noreply.github.com>
2022-04-28 20:03:42 -0500

    ALL FOLDERS IN ORDER (#17)
    Todos los folders se han puesto en orden

---
+++ Assets/Root/Scripts/Bootstrap/Bootstrap.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bootstrap
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public void BootstrapMethod()
+    {
+        //SE CRE GAME MANAGER
+    }
+}
