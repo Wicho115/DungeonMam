154ac4c8e5f8abf829f47dee7bbbfcdf5d91d40b
Dante <61216048+Wicho115@users.noreply.github.com>
2022-05-01 20:46:10 -0500

    UI IMPLEMENTED (#19)
    * ALL FOLDERS IN ORDER
    
    Todos los folders se han puesto en orden
    
    * Ui Implemented
    
    debido a que el equipo de UI no pudo subirlo a github, he decidio pedirles el paquete de unity e implementarla en mi proyecto para tener todo en regla

---
+++ Assets/Root/UI/Menu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CargarEscena(string nombre)
+    {
+        SceneManager.LoadScene(nombre);
+        
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
