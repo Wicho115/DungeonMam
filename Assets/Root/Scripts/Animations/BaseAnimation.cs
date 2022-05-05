9d5d465044bbf6c66cc1902058f81d125ebaac3d
Dante <61216048+Wicho115@users.noreply.github.com>
2022-05-04 20:21:11 -0500

    Modelos (#24)
    * ALL FOLDERS IN ORDER
    
    Todos los folders se han puesto en orden
    
    * Ui Implemented
    
    debido a que el equipo de UI no pudo subirlo a github, he decidio pedirles el paquete de unity e implementarla en mi proyecto para tener todo en regla
    
    * IA enemigos (#20)
    
    Cree 2 escenas de prueba para testear la IA de enemigos. El enemigo base puede perseguir al jugador, puede subir escaleras, saltar de superficies altas para alcanzar al jugador más rápido. Y también puede diferenciar entre zonas que puede o no puede pasar (Ejemplo= El esqueleto no puede pasar por el agua pero el fantasma puede pasar). También hice un enemigo a distancia, este perseguirá al jugador hasta que entre en el rango de ataque. Una vez en rango de ataque se quedará quieto y empezara a disparar al jugador cada cierto tiempo.
    
    Co-authored-by: DomnhallIvan <cdmx2125@amerike.edu.mx>
    
    * Delete Scripts.meta
    
    Se elimino scripts.
    
    * Modelos - Pirata
    
    Se ha colocado en el proyecto el modelo, tambien se añadió un prefab del modelo del personaje
    
    * Some Models Added
    
    Modelos han sido añadidos a la escena de "PruebasModelos"
    
    Co-authored-by: DomnhallIvan <cdmx2125@amerike.edu.mx>

---
+++ Assets/Root/Scripts/Animations/BaseAnimation.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private List<AnimFrame> _frames = new List<AnimFrame>();
+
+    public List<AnimFrame> frames => _frames;
+
+    public Coroutine animCoroutine { get; protected set; } = null;
+
+    public void DoAnimation()
+    {
+        if (frames.Count == 0)
+        {
+            Debug.LogError("There are no frames on the list");
+            return;
+        }
+        if (animCoroutine is null) animCoroutine = StartCoroutine(BaseAnimationCoroutine());
+    }
+
+    private IEnumerator BaseAnimationCoroutine()
+    {
+        
+        yield return StartCoroutine(AnimationCoroutine());
+        animCoroutine = null;
+        DoAnimation();
+    }
+
+    protected virtual IEnumerator AnimationCoroutine()
+    {
+        for (int i = 0; i < frames.Count; i++)
+        {
+            frames[i].ActivateFrame();
+            yield return new WaitForSeconds(frames[i].timeFrame);
+            frames[i].DeactivateFrame();
+        }
+    }
+}
